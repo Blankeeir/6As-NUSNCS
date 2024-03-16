d957c45f23dded75e8c55c4a72044f21f077e4d2
yiwen101 <e0968880@u.nus.edu>
2024-03-17 00:12:20 +0800

    Test and debug run_dynamic_data.py

--- run_dynamic_data.py
+++ run_dynamic_data.py
@@ -13,8 +13,11 @@ load_dotenv()
 AccountKey = os.getenv("ACCOUNT_KEY")
 accept = 'application/json'
 _headers = {'AccountKey': AccountKey, 'accept': accept} 
+pwd = os.path.dirname(os.path.abspath(__file__))
+print(pwd)
+
 def requestAndWriteToFile(url, file_name, relative_file_path, withLink=False):
-    folder_path = os.path.join(os.path.dirname(__file__), relative_file_path)
+    folder_path = os.path.join(pwd, relative_file_path)
     os.makedirs(folder_path, exist_ok=True)
     file_path = os.path.join(folder_path, file_name)
     res = requests.get(url, headers=_headers)
@@ -23,7 +26,13 @@ def requestAndWriteToFile(url, file_name, relative_file_path, withLink=False):
         res = requests.get(link, headers=_headers)
     with open(file_path, 'w') as file:
         json.dump(res.json(), file)
-   
+apiMock = {
+    "interval": 10,
+    "url": "https://api.data.gov.sg/v1/environment/2-hour-weather-forecast",
+    "name": "rainfall"
+}
+#requestAndWriteToFile('https://api.data.gov.sg/v1/environment/2-hour-weather-forecast', 'rainfall.json', 'data/dynamic/rainfall')
+
 def update(api):
     interval = api['interval']
     url = api['url']
@@ -35,20 +44,20 @@ def update(api):
     params = [""]
     if 'params' in api:
         params = api['params']
-    while True:
-        for param in params:
-            _url = url + param
-            def temp():
-                while True:
-                    requestAndWriteToFile(_url, file_name, relative_file_path, withLink)
-                    time.sleep(interval)
-            t = threading.Thread(target=temp)
-            threads.append(t)
-            t.start()
+    def temp():
+        while True:
+            print("Updating " + file_name)
+            requestAndWriteToFile(url, file_name, relative_file_path, withLink)
+            time.sleep(interval)
+    t = threading.Thread(target=temp)
+    threads.append(t)
+    t.start()
 
 apis = None
 with open("data/dynamic/apis.json", 'r') as file:
     apis = json.load(file)
-
+update(apiMock)
+'''
 for api in apis:
     update(api)
+'''
