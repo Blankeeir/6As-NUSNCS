9aa3b8b28c01094914073cd86f75a5e7f932732c
Michael Kang <51043502+onepersonhere@users.noreply.github.com>
2024-03-18 15:01:11 +0800

    Init commit

---
+++ src/Controller/controller.py
@@ -0,0 +1,23 @@
+import requests
+import json
+
+# Functions to get the static data from ../../data folder
+
+# Functions to get the inputs from the website (api endpoint, get inputs as JSON)
+# Inputs are as such: {
+#    "Prompt"
+#    "Max_Tokens"
+#    "Temperature"
+#    "Top_P"
+#    "Frequency_Penalty"
+#    "Presence_Penalty"
+#    "Stop"
+#    "Echo"
+#}
+
+# Functions to get the real time data from ../model/data folder
+
+# Functions to get the AI Model's response data from ../model/openai folder
+
+# Functions to fine tune the model with the given data
+
