00f9fa8eb1c61750d9bfcc0016870215a9a9bc9c
Siyi Xu <emmaxu2005@gmail.com>
2024-03-18 21:01:03 +0800

    .

--- chatgtAssisstant.py
+++ chatgtAssisstant.py
@@ -1,11 +1,11 @@
 ### This py implements assistant api
 from openai import OpenAI
-from 
+from ChatCompletion.py
 
 client = OpenAI()
   
 assistant = client.beta.assistants.create(
-  name="Math Tutor",
+  name="testing assistant",
   instructions="You are a personal math tutor. Write and run code to answer math questions.",
   tools=[{"type": "code_interpreter"}],
   model="gpt-4-turbo-preview",
