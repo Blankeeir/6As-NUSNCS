e2555f8c2043d7fa402357605738d81f6c61cbdd
Siyi Xu <e1300538@u.nus.edu>
2024-03-19 20:01:14 +0800

    Update chatgtAssisstant.py

--- src/model/OpenAiModel/chatgtAssisstant.py
+++ src/model/OpenAiModel/chatgtAssisstant.py
@@ -1,45 +1,32 @@
 ### This py implements assistant api
 from openai import OpenAI
 from chatCompletion import ChatCompletion
-
-#### If request manually:
-'''import requests
-
-headers = {
-    "OpenAI-Beta": "assistants=v1",
-    "Authorization": "Bearer YOUR_OPENAI_API_KEY"
-}
-
-response = requests.post("https://api.openai.com/v1/assistants", headers=headers)
-
-# Handle the response...'''
-
-
-
-
-client = OpenAI()
+from finetune import *
+from imageGeneration import *
+from dynamicPricing import *
+### instantiate an assistant
 
 
 class Assisstant(object):
-     def __init__(self,api_key,assistant_name):
-         super().__init__()
-         self.api_key = api_key
-         self.assisstant_name = assistant_name
+    def __init__(self, assistant_name):
+        super().__init__()
+        client = CLIENT
+        self.name = assistant_name
+        self.thread = client.beta.threads.create()
+        self.assistant = client.beta.assistants.create(
+        name="testing assistant",
+        instructions="user instructions here",
+        tools=[{"type": "code_interpreter"}],
+        model="gpt-4-turbo-preview",
+        )   
 
 
-assistant = client.beta.assistants.create(
-  name="testing assistant",
-  instructions="user instructions here",
-  tools=[{"type": "code_interpreter"}],
-  model="gpt-4-turbo-preview",
-)
 
-thread = client.beta.threads.create()
+            ### assisstant create user message
+    def createMessage(self, message):
+        message = self.client.beta.threads.messages.create(
+        thread_id = self.thread.id,
+        role ="user",
+        content = message
+    )
 
-
-message = client.beta.threads.messages.create(
-    thread_id=thread.id,
-    role="user",
-    content="say hello"
-
-)
