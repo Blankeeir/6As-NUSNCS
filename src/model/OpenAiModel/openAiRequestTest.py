a93d9be7079d2363e25b366f99e608fe4c5cafcd
Siyi Xu <e1300538@u.nus.edu>
2024-03-19 22:31:50 +0800

    Update openAiRequestTest.py

--- src/model/OpenAiModel/openAiRequestTest.py
+++ src/model/OpenAiModel/openAiRequestTest.py
@@ -9,15 +9,13 @@ from dynamicPricing import *
 
 if __name__ == '__main__':
     requestor = ChatCompletion()
+    input_s = input('user input: ')
+    requestor.get_chat_response(input_s)
+    '''print(res)
 
-    while 1:
-        input_s = input('user input: ')
-        requestor.get_chat_response(input_s)
-        '''print(res)
+    response = res
 
-        response = res
-
-        print(f"chatGPT: {response}")'''
+    print(f"chatGPT: {response}")'''
 
         # To get number of tokens
         # print(num_tokens_from_messages(messages, MODEL))
