6b35b4b7d1895dfb7d8ce1e350203f9a86dc6f0d
Siyi Xu <e1300538@u.nus.edu>
2024-03-19 21:01:56 +0800

    .

--- src/model/OpenAiModel/finetune.py
+++ src/model/OpenAiModel/finetune.py
@@ -7,12 +7,15 @@ import os
 ### Finetune using clear instructions:
     #https://platform.openai.com/docs/guides/prompt-engineering/strategy-write-clear-instructions
 MODEL = "gpt-4-1106-preview"
+CLIENT = OpenAI(
+            # This is the default and can be omitted
+            api_key=os.environ.get("OPENAI_API_KEY")  # see .env
+        )
 SYSTEM = "When I ask for help to give me an optimal route from my current location to my destination, \
     you should reply concisely with the optimized route and their"
 USER = "please provide an optimiized route with my current location delimited by the first pair of triple quotes and "
 ASSISSTANT = "Example: "
-
-
+TOOLS = []
 
 
 
