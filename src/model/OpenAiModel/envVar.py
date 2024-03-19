2a75c6a78eb3823b53295a3003d1a633632e2cc8
Siyi Xu <e1300538@u.nus.edu>
2024-03-20 01:37:41 +0800

    update openai model

--- src/model/OpenAiModel/envVar.py
+++ src/model/OpenAiModel/envVar.py
@@ -15,37 +15,17 @@ CLIENT = OpenAI(
 SYSTEM = "When I ask for help to give me an optimal route from my current location to my destination, \
     you should reply concisely with the optimized route and their"
 USER = "please provide an optimiized route with my current location delimited by the first pair of triple quotes and "
-ASSISSTANT = "Example: "
-TOOLS = []
-
-
-
-
-
-
-
+EXAMPLE = "Example: "
+ASSISSTANT_INSTRUCTION = ''
+TOOLS = [{"type": "code_interpreter"}
+         , ]
 
 # Code here for file upload (fine-tuning purpose)
 
+# Code here for dynamic pricing model
+
+# Code here for image upload
 
 
-class finetuneWithFile(object):
-    def __init__(self,training_file,model):
-        super().__init__()
-        self.training_file = training_file
-        self.model = model
-
-    def create_finetune(self):
-        client = OpenAI(
-            # This is the default and can be omitted
-            api_key= os.environ.get("OPENAI_API_KEY")  # see .env
-        )
-
-        response = client.fine_tuning.jobs.create(
-            training_file=self.training_file,
-            model=self.model
-        )
-
-        return response
 
 
