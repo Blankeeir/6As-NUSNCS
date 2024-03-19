a1c156c611c79c5b6cfddb5d47b1ec8d956f4e17
Siyi Xu <e1300538@u.nus.edu>
2024-03-19 17:25:08 +0800

    .

--- src/model/OpenAiModel/ChatCompletion.py
+++ src/model/OpenAiModel/ChatCompletion.py
@@ -3,7 +3,7 @@ from openai import OpenAI
 import os
 import json
 from dotenv import load_dotenv
-load_dotenv()
+load_dotenv('.env.dev')
 
 class ChatCompletion(object):
     def __init__(self, model_name = 'gpt-3.5-turbo'):
@@ -41,8 +41,8 @@ class ChatCompletion(object):
         return res
 
 
-#test = ChatCompletion()
-#print(test.get_chat_response("haha"))
+test = ChatCompletion()
+print(test.get_chat_response("haha"))
 ## save time by streaming the response
 ## https://beta.openai.com/docs/guides/streaming/
 
